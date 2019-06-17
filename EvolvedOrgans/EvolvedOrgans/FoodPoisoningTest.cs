e4768875c5165114485e982473a0b6d6f40b56d2
jetharius <jetharius@gmail.com>
2019-06-16 21:25:09 -0600

    ALL THE HARMONY w/ VS SOLUTION

---
+++ EvolvedOrgans/EvolvedOrgans/FoodPoisoningTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace EvolvedOrgans
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.evolvedorgans");
+
+            MethodInfo targetmethod = AccessTools.Method(typeof(RimWorld.FoodUtility), "AddFoodPoisoningHediff");
+
+            HarmonyMethod prefixmethod = new HarmonyMethod(typeof(EvolvedOrgans.HarmonyPatches).GetMethod("AddFoodPoisoningHediff_Prefix"));
+
+            harmony.Patch(targetmethod, prefixmethod, null);
+        }
+
+        public static void AddFoodPoisoningHediff_Prefix(Pawn pawn)
+        {
+            if (pawn.health.immunity.GetImmunity(HediffDefOf.FoodPoisoning) > 0f)
+            {
+                return;
+            }
+        }
+    }
+}
