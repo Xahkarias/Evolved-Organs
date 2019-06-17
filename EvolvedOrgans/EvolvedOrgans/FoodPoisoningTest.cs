46c77a3c1f641e62ecb04f5dede5e69a7df53542
jetharius <jetharius@gmail.com>
2019-06-17 04:03:43 -0600

    WORKING HARMONY PATCH BRUH!

--- EvolvedOrgans/EvolvedOrgans/FoodPoisoningTest.cs
+++ EvolvedOrgans/EvolvedOrgans/FoodPoisoningTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Reflection;
+﻿using System.Reflection;
 using Harmony;
 using RimWorld;
 using Verse;
@@ -11,10 +6,8 @@ using Verse;
 namespace EvolvedOrgans
 {
     [StaticConstructorOnStartup]
-    static class HarmonyPatches
-    {
-        static HarmonyPatches()
-        {
+    static class HarmonyPatches{
+        static HarmonyPatches(){
             HarmonyInstance harmony = HarmonyInstance.Create("rimworld.evolvedorgans");
 
             MethodInfo targetmethod = AccessTools.Method(typeof(RimWorld.FoodUtility), "AddFoodPoisoningHediff");
@@ -22,14 +15,16 @@ namespace EvolvedOrgans
             HarmonyMethod prefixmethod = new HarmonyMethod(typeof(EvolvedOrgans.HarmonyPatches).GetMethod("AddFoodPoisoningHediff_Prefix"));
 
             harmony.Patch(targetmethod, prefixmethod, null);
+            Log.Message("Harmony successfully patched in Evolved Organ Methods", false);
         }
 
-        public static void AddFoodPoisoningHediff_Prefix(Pawn pawn)
+        public static bool AddFoodPoisoningHediff_Prefix(Pawn pawn)
+            //do not run AddFoodPoisonHediff if pawn has immunity to food poisoning
         {
-            if (pawn.health.immunity.GetImmunity(HediffDefOf.FoodPoisoning) > 0f)
-            {
-                return;
-            }
+            if (pawn.health.immunity.GetImmunity(HediffDefOf.FoodPoisoning) > 0f){
+                //Log.Message("Food poisoning set to immunizable will not get hediff added", true);
+                return false;
+            } return true;
         }
     }
 }
